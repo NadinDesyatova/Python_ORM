69355f15339a5f67b622df9cab3763acd72136b4
NadinDesyatova <nadindinina@yandex.ru>
2024-01-13 23:17:25 +0300

    File "База данных существует" added

--- main.py
+++ main.py
@@ -5,84 +5,58 @@ from sqlalchemy.orm import sessionmaker
 from models import create_tables, Publisher, Book, Shop, Stock, Sale
 
 
-connection_driver = 'postgresql'
-user='postgres'
-password=''
-server_name = 'localhost'
-server_port = '5432'
-db_name = 'book_sales_db'
+def read_data_file(file_path):
+    with open(file_path, "r") as f:
+        data = json.load(f)
+
+    return data    
+
+def insert_data(session, data):
+    for item in data:
+        model = {
+            'publisher': Publisher,
+            'shop': Shop,
+            'book': Book,
+            'stock': Stock,
+            'sale': Sale,
+        }[item.get('model')]
+        session.add(model(id=item.get('pk'), **item.get('fields')))
+    session.commit()
+
+def get_book_sales_information(session, current_publisher):
+    query = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(Shop).join(Sale)
+    if current_publisher.isdigit():
+        query = query.filter(Publisher.id == current_publisher).all()
+    else:
+        query = query.filter(Publisher.name == current_publisher).all()
+
+    for title, name, price, date_sale in query:
+        print(f"{title:<40} | {name:<10} | {price:<8} | {date_sale}")
 
 
-DSN = f"{connection_driver}://{user}:{password}@{server_name}:{server_port}/{db_name}"
-engine = sqlalchemy.create_engine(DSN)
+if __name__ == '__main__':
+    connection_driver = 'postgresql'
+    user='postgres'
+    password=''
+    server_name = 'localhost'
+    server_port = '5432'
+    db_name = 'book_sales_db'
 
-create_tables(engine)
+    DSN = f"{connection_driver}://{user}:{password}@{server_name}:{server_port}/{db_name}"
+    engine = sqlalchemy.create_engine(DSN)
 
+    create_tables(engine)
 
-# def read_data_file(file_path):
-#     with open(file_path, "r") as f:
-#         data = json.load(f)
+    file = "tests_data.json"
+    data_to_insert = read_data_file(file)
+
+    current_publisher = input("Введите имя или идентификатор издателя: ")
+
+    Session = sessionmaker(bind=engine)
+    current_session = Session()
     
+    insert_data(current_session, data_to_insert)
 
-# def insert_data(session, data):
-#     for item in data:
-#         if item["model"] == "publisher":
-#             session.add(Publisher(pk_publisher=item["pk"], publisher_name=item["fields"]["name"]))
-#             session.commit()
-#         elif item["model"] == "book":
-#             session.add(Book(pk_book=item["pk"], title=item["fields"]["title"], id_publisher=item["fields"]["id_publisher"]))
-#             session.commit()
-#         elif item["model"] == "shop":
-#             session.add(Shop(pk_shop=item["pk"], shop_name=item["fields"]["name"]))
-#             session.commit()
-#         elif item["model"] == "stock":
-#             session.add(Stock(
-#                 pk_stock=item["pk"], 
-#                 id_shop=item["fields"]["id_shop"], 
-#                 id_book=item["fields"]["id_book"], 
-#                 count=item["fields"]["count"]
-#             ))
-#             session.commit()
-#         elif item["model"] == "sale":
-#             session.add(Sale(
-#                 pk_sale=item["pk"], 
-#                 price=item["fields"]["price"],
-#                 date_sale=item["fields"]["date_sale"],
-#                 id_stock=item["fields"]["id_stock"]
-#             ))
-#             session.commit()
+    get_book_sales_information(current_session, current_publisher)
 
-
-# def get_book_sales_information(session, required_publisher_name=None, required_publisher_id=None):
-#     subq_1 = session.query(Book).join(Publisher.book).filter(
-#         Publisher.publisher_name == required_publisher_name or Publisher.pk_publisher == required_publisher_id
-#     ).subquery("publisher_book")
-#     subq_2 = session.query(Stock).join(subq_1, Stock.id_book == subq_1.c.pk_book).subquery("stock_book")
-#     q = session.query(Sale).join(subq_2, Sale.id_stock == subq_2.c.pk_stock)
-#     for item in q.all():
-#         print(f'{item.title} | {item.publisher_name} | {item.price} | {item.date_sale}\n') 
-
-
-# file = "tests_data.json"
-# data_to_insert = read_data_file(file)
-
-
-# Session = sessionmaker(bind=engine)
-# current_session = Session()
-    
-# insert_data(current_session, data_to_insert)
-
-# current_publisher = input("Введите имя или идентификатор издателя: ")
-
-# get_book_sales_information(current_session, current_publisher, current_publisher)
-
-# current_session.close()
-
-
-# if __name__ == '__main__':
-#     current_connection_driver= input("Введите название драйвера подключения: ")
-#     current_user = input("Введите имя пользователя: ")
-#     current_user_password = input("Введите пароль: ")
-#     current_server_name = input("Введите название сервера: ")
-#     current_server_port = input("Порт подключения: ")
-#     current_db_name = input("Введите название базы данных: ")
+    current_session.close()
--- models.py
+++ models.py
@@ -8,16 +8,16 @@ Base = declarative_base()
 class Publisher(Base):
     __tablename__ = "publisher"
 
-    pk_publisher = sq.Column(sq.Integer, primary_key=True)
-    publisher_name = sq.Column(sq.String(length=80), unique=True, nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=80), unique=True, nullable=False)
 
 
 class Book(Base):
     __tablename__ = "book"
 
-    pk_book = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=80), nullable=False)
-    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.pk_publisher"), nullable=False)
+    id_publisher = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
 
     publisher = relationship(Publisher, backref="book")
 
@@ -25,18 +25,17 @@ class Book(Base):
 class Shop(Base):
     __tablename__ = "shop"
 
-    pk_shop = sq.Column(sq.Integer, primary_key=True)
-    shop_name = sq.Column(sq.String(length=40), unique=True, nullable=False)
+    id = sq.Column(sq.Integer, primary_key=True)
+    name = sq.Column(sq.String(length=40), unique=True, nullable=False)
 
 
 class Stock(Base):
     __tablename__ = "stock"
 
-    
-    pk_stock = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True)
     count = sq.Column(sq.Integer, nullable=False)
-    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.pk_shop"), nullable=False)
-    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.pk_book"), nullable=False)
+    id_shop = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
+    id_book = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
 
     shop = relationship(Shop, backref="stock")
     book = relationship(Book, backref="stock")
@@ -45,11 +44,11 @@ class Stock(Base):
 class Sale(Base):
     __tablename__ = "sale"
 
-    pk_sale = sq.Column(sq.Integer, primary_key=True)
+    id = sq.Column(sq.Integer, primary_key=True)
     price = sq.Column(sq.Float(2), nullable=False)
     date_sale = sq.Column(sq.Date, nullable=False)
     count = sq.Column(sq.Integer, nullable=False)
-    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.pk_stock"), nullable=False)
+    id_stock = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
 
     stock = relationship(Stock, backref="sale")
 
